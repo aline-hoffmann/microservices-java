3596bdc59dec575885377e6c8b595624ee024576
aline-hoffmann <alinehoffmannp@gmail.com>
2025-09-09 19:05:43 -0300

    atividade avaliativa

---
+++ greeting-service/src/main/java/br/edu/atitus/greeting_service/controllers/GreetingController.java
@@ -0,0 +1,52 @@
+package br.edu.atitus.greeting_service.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.edu.atitus.greeting_service.configs.GreetingConfig;
+
+@RestController
+@RequestMapping("greeting")
+public class GreetingController {
+	
+	//@Value("${greeting-service.greeting}")
+	//private String greeting;
+	//@Value("${greeting-service.default-name}")
+	//private String defaultName;
+	
+	private final GreetingConfig config;
+	
+	public GreetingController(GreetingConfig config) {
+		super();
+		this.config = config;
+	}
+
+	@GetMapping
+	public ResponseEntity<String> greet(
+			@RequestParam(required = false) String name){
+		String greetingReturn = config.getGreeting();
+		String nameReturn = name != null ? name : config.getDefaultName();
+		String textReturn = String.format("%s, %s!!!", greetingReturn, nameReturn);
+		
+		return ResponseEntity.ok(textReturn);
+	}
+	
+	@GetMapping("/{name}")
+	public ResponseEntity<String> greetPath(
+			@PathVariable String name){
+		String greetingReturn = config.getGreeting();
+		String nameReturn = name != null ? name : config.getDefaultName();
+		return ResponseEntity.ok(String.format("%s, %s!!!", greetingReturn, nameReturn));
+	}
+	
+	public String getMethodName(@RequestParam String param) {
+		return new String();
+	}
+	
+
+}
