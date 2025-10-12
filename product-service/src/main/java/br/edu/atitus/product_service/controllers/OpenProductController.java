a5e8435c6f3b03132c2bd9084282c177ad79f973
aline-hoffmann <alinehoffmannp@gmail.com>
2025-10-12 11:05:53 -0300

    api externa, circuit breaker e cache

--- product-service/src/main/java/br/edu/atitus/product_service/controllers/OpenProductController.java
+++ product-service/src/main/java/br/edu/atitus/product_service/controllers/OpenProductController.java
@@ -7,19 +7,22 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.edu.atitus.product_service.clients.CurrencyClient;
+import br.edu.atitus.product_service.clients.CurrencyResponse;
 import br.edu.atitus.product_service.entities.ProductEntity;
 import br.edu.atitus.product_service.repositories.ProductRepository;
 
 @RestController
-
 @RequestMapping("products")
 public class OpenProductController {
 	
 	private final ProductRepository repository;
+	private final CurrencyClient currencyClient;
 
-	public OpenProductController(ProductRepository repository) {
+	public OpenProductController(ProductRepository repository, CurrencyClient currencyClient) {
 		super();
 		this.repository = repository;
+		this.currencyClient = currencyClient;
 	}
 	
 	@Value("${server.port}")
@@ -29,16 +32,26 @@ public class OpenProductController {
 	public ResponseEntity<ProductEntity> getProduct(
 			@PathVariable Long idProduct,
 			@PathVariable String targetCurrency
-			) throws Exception {
+ 			) throws Exception {
 		
 		ProductEntity product = repository.findById(idProduct)
 				.orElseThrow(() -> new Exception("Product not found"));
 		
-		product.setEnviroment("Product-Service running on Port: " + serverPort);
-		product.setConvertedPrice(product.getPrice()); // MOCK = > somente para testes
+		product.setEnviroment("Product-service running on Port: " + serverPort);
+		
+		if (targetCurrency.equalsIgnoreCase(product.getCurrency()))
+			product.setConvertedPrice(product.getPrice());
+		else {
+			CurrencyResponse currency = currencyClient.getCurrency(
+					product.getPrice(), 
+					product.getCurrency(), 
+					targetCurrency);
+			product.setConvertedPrice(currency.getConvertedValue()); // MOCK =>testes
+			product.setEnviroment(product.getEnviroment() +
+					" - " + currency.getEnviroment());
+		}
 		
 		return ResponseEntity.ok(product);
-		
 	}
 
 }
